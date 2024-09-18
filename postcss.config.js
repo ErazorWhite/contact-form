bd36ff01e89b03c736f128c1eb422638d41d5018
hoi <hoi@servers.com>
2024-09-18 12:04:00 +0300

    Configured PostCSS (nested & custom media); +css color vars; +modern-normalize

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = (ctx) => ({
+    map: ctx.env === 'development' ? ctx.map : false,
+    plugins: {
+        '@csstools/postcss-global-data': {
+            files: ['./src/index.css'],
+        },
+        'postcss-custom-media': {},
+        'postcss-nested': {preserveEmpty: true},
+        cssnano: ctx.env === 'production' ? {} : false
+    }
+})
