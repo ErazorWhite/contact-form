b4c45090db4fa69679488b2a8f017d19b07c2016
hoi <hoi@servers.com>
2024-09-20 13:05:52 +0300

    Toasted added on the top of the app

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { ContactUsForm } from './components/ContactUsForm/ContactUsForm.jsx';
 import { Container } from './components/Container/Container.jsx';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Toaster />
     <Container>
       <ContactUsForm />
     </Container>
