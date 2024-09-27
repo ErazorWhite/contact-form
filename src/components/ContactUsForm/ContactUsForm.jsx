4806f47e5b61aaf6db867608b255d1956b695c3d
hoi <hoi@servers.com>
2024-09-27 18:11:04 +0300

    initialValues & initialValues moved out of ContactUsForm scope

--- src/components/ContactUsForm/ContactUsForm.jsx
+++ src/components/ContactUsForm/ContactUsForm.jsx
@@ -9,39 +9,44 @@ import { FormTextArea } from '../FormTextArea/FormTextArea.jsx';
 import toast from 'react-hot-toast';
 import { Toast } from '../Toast/Toast.jsx';
 
+const initialValues = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    queryType: '',
+    message: '',
+    acceptedTerms: false,
+};
+
+const validationSchema = Yup.object({
+    firstName: Yup.string()
+        .max(15, 'Must be 15 characters or less')
+        .required('This field is required'),
+    lastName: Yup.string()
+        .max(20, 'Must be 20 characters or less')
+        .required('This field is required'),
+    email: Yup.string()
+        .email('Invalid email address')
+        .required('This field is required'),
+    queryType: Yup.string().required('Please select a query type'),
+    message: Yup.string().required('This field is required'),
+    acceptedTerms: Yup.boolean()
+        .required('Required')
+        .oneOf(
+            [true],
+            'To submit this form, please consent to being contacted'
+        ),
+})
+
 export const ContactUsForm = () => {
-  return (
+
+    return (
     <div className={css.formThumb}>
       <h1 className={css.textHeader}>Contact Us</h1>
 
       <Formik
-        initialValues={{
-          firstName: '',
-          lastName: '',
-          email: '',
-          queryType: '',
-          message: '',
-          acceptedTerms: false,
-        }}
-        validationSchema={Yup.object({
-          firstName: Yup.string()
-            .max(15, 'Must be 15 characters or less')
-            .required('This field is required'),
-          lastName: Yup.string()
-            .max(20, 'Must be 20 characters or less')
-            .required('This field is required'),
-          email: Yup.string()
-            .email('Invalid email address')
-            .required('This field is required'),
-          queryType: Yup.string().required('Please select a query type'),
-          message: Yup.string().required('This field is required'),
-          acceptedTerms: Yup.boolean()
-            .required('Required')
-            .oneOf(
-              [true],
-              'To submit this form, please consent to being contacted'
-            ),
-        })}
+        initialValues={initialValues}
+        validationSchema={validationSchema}
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
             setSubmitting(false);
