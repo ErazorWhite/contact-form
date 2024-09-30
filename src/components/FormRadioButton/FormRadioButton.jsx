ee5eca39a33cfa396a9197a4933e0defed86524f
hoi <hoi@servers.com>
2024-09-30 18:00:46 +0300

    radio input extracted from FormRadioGroup to FormRadioButton dedicated component

---
+++ src/components/FormRadioButton/FormRadioButton.jsx
@@ -0,0 +1,28 @@
+import clsx from 'clsx';
+import css from '../FormRadioGroup/FormRadioGroup.module.css';
+import { IconRadiobuttonChecked } from '../icons/IconRadiobuttonChecked.jsx';
+import { IconRadiobuttonUnchecked } from '../icons/IconRadiobuttonUnchecked.jsx';
+import React from 'react';
+
+export const FormRadioButton = (props) => (
+  <label
+    htmlFor={props.option.value}
+    className={clsx(
+      css.inputText,
+      css.radioThumb,
+      props.checked && css.radioHighlight
+    )}
+  >
+    <input
+      type="radio"
+      className="visually-hidden"
+      id={props.option.value}
+      name={props.field.name}
+      value={props.option.value}
+      onChange={props.field.onChange}
+      onBlur={props.field.onBlur}
+    />
+    {props.checked ? <IconRadiobuttonChecked /> : <IconRadiobuttonUnchecked />}
+    {props.option.label}
+  </label>
+);
--- src/components/FormRadioGroup/FormRadioGroup.jsx
+++ src/components/FormRadioGroup/FormRadioGroup.jsx
@@ -1,9 +1,7 @@
 import { useField } from 'formik';
 import css from './FormRadioGroup.module.css';
-import clsx from 'clsx';
 import React from 'react';
-import { IconRadiobuttonUnchecked } from '../icons/IconRadiobuttonUnchecked.jsx';
-import { IconRadiobuttonChecked } from '../icons/IconRadiobuttonChecked.jsx';
+import {FormRadioButton} from "../FormRadioButton/FormRadioButton.jsx";
 
 export const FormRadioGroup = ({ groupName, options, ...props }) => {
   const [field, meta] = useField({ ...props });
@@ -20,26 +18,7 @@ export const FormRadioGroup = ({ groupName, options, ...props }) => {
           const isChecked = field.value === option.value;
           return (
             <li key={option.value} className={css.radioItem}>
-              <label
-                htmlFor={option.value}
-                className={clsx(css.inputText, css.radioThumb, isChecked && css.radioHighlight)}
-              >
-                <input
-                  type="radio"
-                  className="visually-hidden"
-                  id={option.value}
-                  name={field.name}
-                  value={option.value}
-                  onChange={field.onChange}
-                  onBlur={field.onBlur}
-                />
-                {isChecked ? (
-                  <IconRadiobuttonChecked />
-                ) : (
-                  <IconRadiobuttonUnchecked />
-                )}
-                {option.label}
-              </label>
+              <FormRadioButton option={option} checked={isChecked} field={field} />
             </li>
           );
         })}
