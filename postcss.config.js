68ce4892fb8b5da25413d3e762db400cc4fcc08d
hoi <hoi@servers.com>
2024-09-30 11:14:37 +0300

    fonts fix via mixins

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 export default (ctx) => ({
     map: ctx.env === 'development' ? ctx.map : false,
     plugins: {
+        'postcss-mixins': {},
         '@csstools/postcss-global-data': {
             files: ['./src/index.css'],
         },
