263ee1e55ab908f9a4487c59ec338ab60d4f5529
hoi <hoi@servers.com>
2024-09-30 17:47:33 +0300

    removed redundant div & used useID

--- src/components/FormCheckbox/FormCheckbox.jsx
+++ src/components/FormCheckbox/FormCheckbox.jsx
@@ -1,31 +1,25 @@
 import { useField } from 'formik';
 import css from './FormCheckbox.module.css';
 import { IconCheckboxUnchecked } from '../icons/IconCheckBoxUnchecked.jsx';
-import {IconCheckboxChecked} from "../icons/IconCheckboxChecked.jsx";
+import { IconCheckboxChecked } from '../icons/IconCheckboxChecked.jsx';
+import { useId } from 'react';
 
 export const FormCheckbox = ({ children, ...props }) => {
   const [field, meta] = useField({ ...props, type: 'checkbox' });
+  const id = useId(); // Генерируем уникальный идентификатор
 
   return (
     <div className={css.checkboxThumb}>
-      <div>
-          <input
-              className="visually-hidden"
-              id={props.id}
-              type="checkbox"
-              {...field}
-          />
-          <label htmlFor={field.name} className={css.checkboxLabel}>
+      <input className="visually-hidden" id={id} type="checkbox" {...field} />
+      <label htmlFor={id} className={css.checkboxLabel}>
         <span className={css.checkboxMark}>
-          {field.checked ? <IconCheckboxChecked/> : <IconCheckboxUnchecked />}
+          {field.checked ? <IconCheckboxChecked /> : <IconCheckboxUnchecked />}
         </span>
-              {children}
-          </label>
-          {meta.touched && meta.error ? (
-              <div className="error">{meta.error}</div>
-          ) : null}
-      </div>
+        {children}
+      </label>
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
     </div>
   );
 };
-
