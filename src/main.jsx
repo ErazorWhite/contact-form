786ba9b78329e1f7b5a7a76d89cd06801f28a7aa
hoi <hoi@servers.com>
2024-09-30 17:38:09 +0300

    Container removed as component

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { ContactUsForm } from './components/ContactUsForm/ContactUsForm.jsx';
-import { Container } from './components/Container/Container.jsx';
 import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Toaster />
-    <Container>
-      <ContactUsForm />
-    </Container>
+    <div className="container">
+      <ContactUsForm/>
+    </div>
   </StrictMode>
 );
