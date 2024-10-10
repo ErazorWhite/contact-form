b13b635afbacc73d29e1f5efe3c0d61f1d70fd6a
hoi <hoi@servers.com>
2024-10-10 13:04:50 +0300

    unnecessary propTypes removed

--- src/components/form/ContactUsForm/ContactUsForm.jsx
+++ src/components/form/ContactUsForm/ContactUsForm.jsx
@@ -3,7 +3,6 @@ import { Formik } from 'formik';
 import { ContactUsFormFields } from '../ContactUsFormFields/ContactUsFormFields.jsx';
 import { contactUsFormValidationSchema } from '../../../utilities/validationSchemas.js';
 import { notifyExternally } from '../../../utilities/notifications/index.js';
-import PropTypes from "prop-types";
 
 const initialValues = {
   firstName: '',
@@ -25,31 +24,15 @@ const handleSubmit = (values, { setSubmitting, resetForm }) => {
   }, 750);
 };
 
-export const ContactUsForm = () => {
-  return (
-    <div className={css.formThumb}>
-      <h1 className={css.textHeader}>Contact Us</h1>
+export const ContactUsForm = () => (
+  <div className={css.formThumb}>
+    <h1 className={css.textHeader}>Contact Us</h1>
 
-      <Formik
-        initialValues={initialValues}
-        validationSchema={contactUsFormValidationSchema}
-        onSubmit={handleSubmit}
-        component={ContactUsFormFields}
-      />
-    </div>
-  );
-};
-
-ContactUsForm.propTypes = {
-  initialValues: PropTypes.shape({
-    firstName: PropTypes.string.isRequired,
-    lastName: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    queryType: PropTypes.string.isRequired,
-    message: PropTypes.string.isRequired,
-    acceptedTerms: PropTypes.bool.isRequired,
-  }).isRequired,
-  validationSchema: PropTypes.object.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  notifyExternally: PropTypes.func.isRequired,
-};
+    <Formik
+      initialValues={initialValues}
+      validationSchema={contactUsFormValidationSchema}
+      onSubmit={handleSubmit}
+      component={ContactUsFormFields}
+    />
+  </div>
+);
